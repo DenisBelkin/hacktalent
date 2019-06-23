54d30e0520e2b226b56e45e72d83814688dff4f5
DenisBelkin <dan.98belkin@gmail.com>
2019-06-23 11:45:59 +0300

    build + refactoring

---
+++ app/build/precache-manifest.4c4d38f099eec7726b516e43e56a36be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888ad47e3f8a730bb770706290753392",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d68406c275fd29199b11",
+    "url": "/static/css/2.0cdb9ae6.chunk.css"
+  },
+  {
+    "revision": "0e9c7f5151a5e2d3108e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d68406c275fd29199b11",
+    "url": "/static/js/2.ce3d6b3c.chunk.js"
+  },
+  {
+    "revision": "0e9c7f5151a5e2d3108e",
+    "url": "/static/js/main.74372f52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
